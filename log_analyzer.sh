19841e53ea42e8ff7553b3b715a7cd4a5f51fcfb
AminurRahman123 <49790468+AminurRahman123@users.noreply.github.com>
2025-01-15 16:59:54 +0000

    Create log_analyzer.sh

---
+++ log_analyzer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <log_file_path>"
+    exit 1
+fi
+
+log=$1
+
+if [ -f "$log" ];then
+        echo "Checking Log File"
+
+        ipaddresses=$(cat $log | awk '{print $1}' | uniq -c | sort -nr | head -5)
+
+        echo "Top 5 IP Addresses with the Most Requests:"
+        echo "$ipaddresses" | awk '{print $2 " - " $1 " requests"}'
+        echo ""
+
+        echo "Top 5 Most Requested Paths: "
+        requests=$(cat $log | awk '{print $6,$7}' | sort | uniq -c | sort -nr | head -5)
+        echo "$requests" | awk '{print $2,$3 " - " $1 " requests"}'
+        echo ""
+
+        echo "Top 5 Response status codes: "
+        responses=$(cat $log | awk '{print $9}' | uniq -c |sort -nr|  head -5)
+        echo "$responses" | awk '{print $2 " - " $1 " requests"}'
+else
+        echo "File not available or cannot be accessed"
+fi
